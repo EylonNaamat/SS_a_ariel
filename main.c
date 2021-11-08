c28807f9edd3ed30410a4667d35a1847a587e8d7
EylonNaamat <eylonnaamat@gmail.com>
2021-11-08 14:52:40 +0200

    new main

--- main.c
+++ main.c
@@ -3,40 +3,40 @@
 int main(){
     int first, last;
     scanf("%d %d", &first, &last);
-    printf("The Armstrong numbers are: ");
+    printf("The Armstrong numbers are:");
     for (int i = first; i <= last; i++)
     {
         if(isArmstrong(i))
         {
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
     printf("\n");
-    printf("The Palindromes are: ");
+    printf("The Palindromes are:");
     for (int i = first; i <= last; i++)
     {
         if(isPalindrome(i))
         {
-            printf("%d ", i);
+            printf(" %d", i);
 
         }
     }
     printf("\n");
-    printf("The Prime numbers are: ");
+    printf("The Prime numbers are:");
     for (int i = first; i <= last; i++)
     {
         if(isPrime(i))
         {
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
     printf("\n");
-    printf("The Strong numbers are: ");
+    printf("The Strong numbers are:");
     for (int i = first; i <= last; i++)
     {
         if(isStrong(i))
         {
-            printf("%d ", i);
+            printf(" %d", i);
 
         }
     }
